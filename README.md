4314363f8b76da4c6edc00516b2cc460c202ff01
Ram Vennam <rvennam987@gmail.com>
2017-02-13 14:14:42 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# PHP Hello World Sample
+
+This application demonstrates a simple, reusable PHP web application.
+
+[![Deploy to Bluemix](https://bluemix.net/deploy/button.png)](https://bluemix.net/deploy?repository=https://github.com/IBM-Bluemix/php-helloworld)
+
+## Run the app locally
+
+1. Download and extract [PHP][]
++ cd into this project's root directory
++ Run `php -S localhost:8000` to start the app using the built-in development web server
++ Access the running app in a browser at <http://localhost:8000>
+
+[PHP]: http://php.net/downloads.php
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Starter Application</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<table>
+		<tr>
+			<td style='width: 30%;'>
+				<img class = 'newappIcon' src='images/newapp-icon.png'>
+			</td>
+			<td>
+				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
+				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
