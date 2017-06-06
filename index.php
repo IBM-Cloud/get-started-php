49f4698c6a0c9860aa63b63c705592960f8ca660
Daniel Grim <degrim@us.ibm.com>
2017-06-06 11:53:40 -0400

    sanitize inputs on server side

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright IBM Corp. 2016
+ * Copyright IBM Corp. 2016-2017
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,49 +21,50 @@
   */
 require 'vendor/autoload.php';
 require_once('./Cloudant.php');
+require_once('./AntiXSS.php');
 $app = new \Slim\Slim();
 $dotenv = new Dotenv\Dotenv(__DIR__);
 try {
-  $dotenv->load();
+    $dotenv->load();
 } catch (Exception $e) {
     error_log("No .env file found");
- }
+}
 $app->get('/', function () {
-  global $app;
+    global $app;
     $app->render('index.html');
 });
 
 $app->get('/api/visitors', function () {
-  global $app;
-  $app->contentType('application/json');
-  $visitors = array();
-  if(Cloudant::Instance()->isConnected()) {
-    $visitors = Cloudant::Instance()->get();
-  }
-  echo json_encode($visitors);
+    global $app;
+    $app->contentType('application/json');
+    $visitors = array();
+    if(Cloudant::Instance()->isConnected()) {
+      $visitors = Cloudant::Instance()->get();
+    }
+    echo json_encode($visitors);
 });
 
 $app->post('/api/visitors', function() {
-	global $app;
-  $visitor = json_decode($app->request()->getBody(), true);
-  if(Cloudant::Instance()->isConnected()) {
-    Cloudant::Instance()->post($visitor);
-    echo sprintf("Hello %s, I've added you to the database!", $visitor['name']);
-  } else {
-    echo sprintf("Hello %s!", $visitor['name']);
-  }
+    global $app;
+    $visitor = AntiXSS::sanitizeInput(json_decode($app->request()->getBody(), true));
+    if(Cloudant::Instance()->isConnected()) {
+      Cloudant::Instance()->post($visitor);
+      echo sprintf("Hello %s, I've added you to the database!", $visitor['name']);
+    } else {
+      echo sprintf("Hello %s!", $visitor['name']);
+    }
 });
 
 $app->delete('/api/visitors/:id', function($id) {
-	global $app;
-	Cloudant::Instance()->delete($id);
+    global $app;
+    Cloudant::Instance()->delete($id);
     $app->response()->status(204);
 });
 
 $app->put('/api/visitors/:id', function($id) {
-	global $app;
-	$visitor = json_decode($app->request()->getBody(), true);
-    echo json_encode(Cloudant::Instance()->put($id, $visitor));
+    global $app;
+    $visitor = json_decode($app->request()->getBody(), true);
+    echo json_encode(Cloudant::Instance()->put($id, AntiXSS::sanitizeInput($visitor)));
 });
 
 $app->run();
